ee5bf7815823bef6126f6e9f36aa8f5c56c20c31
Lukasz <7lukasz8@gmail.com>
2024-02-16 01:49:55 +0100

    spinner

---
+++ src/ui/Spinner.tsx
@@ -0,0 +1,40 @@
+import { motion } from "framer-motion";
+
+export default function Spinner() {
+  return (
+    <div className="flex flex-col gap-1">
+      <motion.div
+        className="w-[150px] h-6 bg-white origin-left"
+        animate={{ scaleX: [0, 1] }}
+        transition={{
+          ease: "linear",
+          repeat: Infinity,
+          duration: 0.6,
+          repeatType: "reverse",
+        }}
+      ></motion.div>
+      <motion.div
+        className="w-[150px] h-6 bg-white origin-left"
+        animate={{ scaleX: [0, 1] }}
+        transition={{
+          ease: "linear",
+          repeat: Infinity,
+          duration: 0.6,
+          repeatType: "reverse",
+          delay: 0.3,
+        }}
+      ></motion.div>
+      <motion.div
+        className="w-[150px] h-6 bg-white origin-left"
+        animate={{ scaleX: [0, 1] }}
+        transition={{
+          ease: "linear",
+          repeat: Infinity,
+          duration: 0.6,
+          repeatType: "reverse",
+          delay: 0.6,
+        }}
+      ></motion.div>
+    </div>
+  );
+}
