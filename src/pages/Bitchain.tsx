b5c0774ab9d73b57ec17c248beb276965de282e3
Lukasz <7lukasz8@gmail.com>
2024-02-17 23:46:17 +0100

    bitchain page

--- src/pages/Bitchain.tsx
+++ src/pages/Bitchain.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
+import ProjectHeader from "../ui/ProjectHeader";
+import AboutProject from "../ui/AboutProject";
+import Footer from "../ui/Footer";
 
 export default function Bitchain() {
   return (
-    <div>Bitchain</div>
-  )
+    <div>
+      <ProjectHeader
+        title="Project Bitchain"
+        desc="Copy of a cryptocurrency exchange containing all the mechanisms of a real exchange"
+        role="/ Front-end Developer"
+        leader={true}
+        context="Group Project at"
+        skni={true}
+        time="in progress for 4 months"
+        img="/bitchain/bitchain.jpg"
+      />
+      <AboutProject
+        desc="Bitchain Project is a cryptocurrency exchange simulator, whose main task is to teach users how to trade, without risk, on fictitious funds. The final project will include all the functionalities of a real stock exchange, at the moment the following have been done: markets, home page, login and registration, individual currency pages, modification of user data, support for an external api for cryptocurrencies and much more."
+        technologies="React, Typescript, Tailwind, Django, Rest API"
+        link="https://github.com/skni-kod/Bitchain-Frontend"
+        deployed={false}
+      />
+      <div className="h-screen"></div>
+      <Footer />
+    </div>
+  );
 }
